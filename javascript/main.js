8b85be8233767ebaa7771f5745236a8ffa368abd
Sina Khalili <khalili@sfu.ca>
2018-10-30 15:24:28 -0700

    update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -64,7 +64,7 @@ var fadeout_tween = TweenMax.to('#toDie', 0.375,{ opacity: 0 });
 	//Commented out, but here's my contact info if you're here
 
 	//$( "#middle" ).click(function() {
-  	//	$("#middle").html("<h3> E-mail: khalili@sfu.ca or sinakhalili@outlook.com or sina@sinakhalili.com <br> Phone:(+1)778-927-6771 <br> Feel free to connect on linkedin by clicking the links below and on github above.  </h3>");
+  	//	$("#middle").html("<h3> E-mail: khalili@sfu.ca or sinakhalili@outlook.com or sina@sinakhalili.com  Feel free to connect on linkedin by clicking the links below and on github above.  </h3>");
 	//});
 
 
